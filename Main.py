9ab38ff3e80fc2c7b6ca7bf2d9d4ef42787a6cc5
Alexandre Potton <alexa@PC-Alexandre>
2025-12-10 15:34:49 +0100

    Atmosphère complétée

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
-
+from constantes.constantes import Constantes
 from avions.Avion import Avion
+from atmosphere.Atmosphere import Atmosphere
+
 
 A320 = Avion("Airbus_A320.csv")
 
@@ -8,3 +10,11 @@ print(A320.getName())
 print(A320.getLength())
 print(A320.getHeight())
 
+print(Constantes.g)
+print(Atmosphere.T0_K)
+
+test  = Atmosphere()
+rho, p, T = test.getRhoPT(15000)
+print("At 15000 m: rho = {:.4f} kg/m³, p = {:.2f} Pa, T = {:.2f} K".format(rho, p, T))
+
+print("Test complete.")
--- atmosphere/Atmosphere.py
+++ atmosphere/Atmosphere.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # Cette ligne ajoute le dossier parent au chemin de recherche de Python
-
 import numpy as np
 from constantes.constantes import Constantes
 
