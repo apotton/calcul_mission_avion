2618c4696fd156fc36423001edc7bd57e0126a4a
Guillaume <guillaumebonneson@gmail.com>
2025-12-10 03:52:09 +0100

    Calcul Cx

--- avions/aero.py
+++ avions/aero.py
@@ -1,6 +1,13 @@
+from constantes.constantes import Constantes 
+from atmosphere.Atmosphere import Atmosphere
+from Avion import Avion
+
 class Aero:
     def __init__(self, avion):
         self.avion = avion
+        self.Current_Cx = 0
+        self.Current_Cz = 0
 
-    
+    def CalculateCz(self,Mach,Pressure):
+        return self.avion.getMaxTakeoffWeight()*Constantes.g/(0.7*Pressure*self.avion.getSref()*Mach**2)
 
