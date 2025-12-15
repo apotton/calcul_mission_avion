272289fb59903faf74171f854338dae9223f1c39
MAYER Pierre <pierre.mayer@etu.unice.fr>
2025-12-15 16:22:23 +0100

    en train de faire le calculateCx

--- avions/aero.py
+++ avions/aero.py
@@ -28,3 +28,26 @@ class Aero:
 
     def getCx(self):
         return self.Cx_t
+    def getMach(self):
+        return self.Mach
+    
+    def CalculateCx(self):
+        #va falloir convertir plein d'angles donc :
+        phi_25_deg = 24.97
+        phi_rad = np.radians(phi_25_deg) 
+        #ules random constante du matlab :
+        l_ref = 6
+        t_to_c_ref = 0.1093
+        #Calcul du nombre de Reynolds
+        Re = (47899*Atmosphere.getP_t()*Aero.getMach()*((1+0.126*Aero.getMach()**2)*Atmosphere.getT_t()+110.4)/(Atmosphere.getT_t()**2))/(1+0.126*Aero.getMach()**2)**(5/2)
+        #Calcul du Cf
+        Cf = 0.455/(1+0.126*Aero.getMach()**2)/(np.log10(Re*l_ref))**2.58
+        #Calcul du K_e & K_c
+        K_e = 4.688*t_to_c_ref**2+3.146*t_to_c_ref
+        le_cosinus = np.cos(phi_rad)**2
+        K_c = 2.859*(Aero.getCz()/le_cosinus)**3-1.849*(Aero.getCz()/le_cosinus)**2 + 0.382*(Aero.getCz()/le_cosinus)+0.06
+        K_phi = 1-0.000178*(phi_rad)**2-0.00065*phi_rad
+        K_i=0.04
+        Delta_Cx_0=0.0115
+        Cx_0_wing=2*Cf*((K_e+K_c)*K_phi+K_i+1)
+        Cx_0=Cx_0_wing+Delta_Cx_0
