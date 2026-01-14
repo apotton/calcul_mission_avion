d361a3a786046c294f9dab8e831223837804f16a
Alexandre Potton <alexa@PC-Alexandre>
2026-01-14 15:21:53 +0100

    Création de la classe Interface

---
+++ interface/Interface.py
@@ -0,0 +1,16 @@
+class Interface:
+    def __init__(self):
+        """Initialize the Interface."""
+        pass
+    
+    def run(self):
+        """Run the interface."""
+        pass
+    
+    def display(self):
+        """Display the interface."""
+        pass
+    
+    def handle_input(self):
+        """Handle user input."""
+        pass
