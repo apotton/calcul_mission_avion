f81e203debc9747ad16046adeac2c1cd1861e56d
fgerard279 <francois.gerard@student.isae-supaero.fr>
2026-01-28 16:07:34 +0100

    changement des classes filles

--- moteurs/Elodie_Roux.py
+++ moteurs/Elodie_Roux.py
@@ -5,12 +5,12 @@ class Elodie_roux(Moteur):
         super().__init__(BPR, OPR)
         # Pas de Donnees_moteur ici, peut-être d'autres constantes spécifiques
 
-    def Calculate_F_MCL_cruise_step(self, Avion):
+    def Calculate_F(self, Avion):
         print("Calcul Poussée via méthode analytique (Elodie Roux)")
         # Ici tu mets tes équations mathématiques
         # self.F_t = ... formule mathématique ...
 
-    def Calculate_SFC_cruise(self, Avion, F_engine_N=None):
+    def Calculate_SFC(self, Avion, F_engine_N=None):
         print("Calcul SFC via méthode analytique (Elodie Roux)")
         # Ici tu mets tes équations mathématiques
         # self.SFC_t = ... formule mathématique ...
@@ -25,4 +25,22 @@ class Elodie_roux(Moteur):
 
 # else % Modèle Elodie Roux
 #     F_MCL_cruise_step_up=2*CalculateFmax(F_0_uni_ref*dx_MCL,Mach_cruise,h_cruise_step+2000*conv_ft_m);
+# end
+
+
+# else % Analytique 
+#     [F_N_AEO_cruise_step]=CalculateFmax(F_0*dx_MCL,Mach_cruise_step,h_cruise_step);
+#     [SFC_cruise_step]=CalculateSFC(F_N_AEO_cruise_step,F_N_AEO_cruise_step,T_cruise_step,Mach_cruise_step,h_cruise_step);
+# end 
+
+
+# Climb ops 
+# else
+#     [F_N_AEO_climb_ops]=CalculateFmax(F_0*dx_MCL,Mach_climb_ops,h_climb_ops);
+#     [SFC_climb_ops]=CalculateSFC(F_N_AEO_climb_ops,F_N_AEO_climb_ops,T_climb_ops,Mach_climb_ops,h_climb_ops);
+# end
+
+# else
+#     [F_N_AEO_climb_ops]=CalculateFmax(F_0*dx_MCL,Mach_climb_ops,h_climb_ops);
+#     [SFC_climb_ops]=CalculateSFC(F_N_AEO_climb_ops,F_N_AEO_climb_ops,T_climb_ops,Mach_climb_ops,h_climb_ops);
 # end
--- moteurs/Moteur.py
+++ moteurs/Moteur.py
@@ -32,10 +32,10 @@ class Moteur:
         return self.F_t
 
 
-    def calculate_F_MCL_cruise_step(self, Avion):
+    def calculate_F(self, Avion):
         pass  # Méthode à implémenter dans les classes filles
 
-    def Calculate_SFC_cruise(self, Avion, F_engine_N=None):
+    def Calculate_SFC(self, Avion, F_engine_N=None):
         pass  # Méthode à implémenter dans les classes filles
 
 
