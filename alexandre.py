4244857467bb5cadc1a9678c28eeaf9385687e92
Alexandre Potton <alexa@PC-Alexandre>
2026-02-10 16:55:47 +0100

    Rien de particulier

--- alexandre.py
+++ alexandre.py
@@ -9,8 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import timeit
 
-A320 = Avion()
-test_atmos  = Atmosphere()
+
 
 
 # # Test du moteur un peu plus en détail: poussée à différentes altitudes et Mach (figure 2D)
@@ -29,9 +28,9 @@ test_atmos  = Atmosphere()
 #         A320.set_h(h_ft * Constantes.conv_ft_m)  # Met à jour l'altitude de l'avion en mètres
 #         A320.Aero.Mach_t = mach                       # Met à jour le Mach de l'avion
 #         A320.Moteur.Calculate_F_Descent()            # Calcule la poussée
-#         # poussees_ligne.append(A320.Moteur.getF() / 1000)  # Stocke la poussée en kN
-#         A320.Moteur.Calculate_SFC_Descent() #(A320.Moteur.getF()/2)            # Calcule la poussée
-#         poussees_ligne.append(A320.Moteur.getSFC() / 1000)  # Stocke la poussée en kN
+#         poussees_ligne.append(A320.Moteur.getF() / 1000)  # Stocke la poussée en kN
+#         # A320.Moteur.Calculate_SFC_Descent() #(A320.Moteur.getF()/2)            # Calcule la poussée
+#         # poussees_ligne.append(A320.Moteur.getSFC() / 1000)  # Stocke la poussée en kN
 #     poussees_2D.append(poussees_ligne)
 
 # # Convertir en numpy array pour faciliter le tracé
@@ -47,11 +46,15 @@ test_atmos  = Atmosphere()
 # ax.set_xlabel('Mach')
 # ax.set_ylabel('Altitude (ft)')
 # plt.show()
+A320 = Avion()
+test_atmos  = Atmosphere()
 
 Enregistrement.enregistrement_descente = True
 Inputs.Aero_simplified = False
 
 # def f():
+#     A320 = Avion()
+#     test_atmos  = Atmosphere()
 #     Montee.Monter(A320, test_atmos)
 #     Enregistrement.reset()
 #     A320.Aero.setMach_t(0.78)
@@ -59,13 +62,12 @@ Inputs.Aero_simplified = False
 #     Descente.Descendre(A320, test_atmos)
 
 # print(timeit.timeit(f, number=1))
+tstart = timeit.default_timer()
+
 
 Enregistrement.reset()
 
-
-tstart = timeit.default_timer()
-
-N = 1
+N = 10
 for i in range(N):
     Montee.Monter(A320, test_atmos)
     A320.setupDescente()
@@ -74,6 +76,7 @@ for i in range(N):
 tend = timeit.default_timer()
 
 temps_moyen = (tend - tstart) / N
+
 print(f"Temps moyen pour une montée + descente : {temps_moyen:.4f} secondes")
 
 print("Distance de descente: " + str(A320.l_descent / 1000) + " km")
